1c8b9fb67ac57cbb7f54356163f1d493df56a1c0
Rhoda27 <151473457+Rhoda27@users.noreply.github.com>
2025-04-30 12:41:45 +0100

    Add files via upload

---
+++ myProject/myNewProject/Program.cs
@@ -0,0 +1,201 @@
+﻿// See https://aka.ms/new-console-template for more information
+  
+//other methods
+string txts = "hello world";
+Console.WriteLine(txts.ToUpper());
+Console.WriteLine(txts.ToLower());
+
+//STRING LENGTH
+string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+Console.WriteLine("The length of the txt string is:" + txt.Length);
+
+//my string length
+
+string text ="abcdefghijkmnopqrstuvwxyz";
+Console.WriteLine("the length of the text string is:" + text.Length);
+
+
+//user input
+Console.WriteLine("user input");
+Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+Console.WriteLine("username is" + username);
+
+Console.WriteLine("Enter age");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("AGE is" + age);
+
+//my user input
+Console.WriteLine("MY user input");
+Console.WriteLine("Enter username");
+string userName = Console.ReadLine();
+Console.WriteLine("username is " + userName);
+
+Console.WriteLine("Enter age");
+int Age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("AGE is" + Age);
+
+
+// conversion method
+
+Console.WriteLine("conversion method");
+int myINT = 10;
+double myDuble = 5.25;
+bool myBol = true;
+
+Console.WriteLine(Convert.ToString(myINT));
+Console.WriteLine(Convert.ToString(myDuble));
+Console.WriteLine(Convert.ToString(myINT));
+Console.WriteLine(Convert.ToString(myBol));
+
+// conversion method
+
+Console.WriteLine("my conversion method");
+int myINTs = 10;
+double myDubles = 5.25;
+bool myBols = true;
+
+Console.WriteLine(Convert.ToString(myINTs));
+Console.WriteLine(Convert.ToString(myDubles));
+Console.WriteLine(Convert.ToString(myINTs));
+Console.WriteLine(Convert.ToString(myBols));
+
+//implicit casting
+Console.WriteLine("Implicit casting");
+int myInt = 9;
+double myDoubl = myInt;
+Console.WriteLine(myInt);
+Console.WriteLine(myDoubl);
+
+//explicit casting
+Console.WriteLine("explicit casting");
+double myDoub = 9.78;
+int myIn = (int) myDoub;
+Console.WriteLine(myDoub);
+Console.WriteLine(myIn); 
+
+//my implicit casting
+Console.WriteLine("my Implicit casting");
+int myInts = 7;
+double myDoubles = myInts;
+Console.WriteLine(myInts);
+Console.WriteLine(myDoubles);
+
+//my  explicit casting
+Console.WriteLine("my explicit casting");
+double myDoubls =4.69;
+int myIns = (int) myDoubls;
+Console.WriteLine(myIns);
+Console.WriteLine(myDoubls);
+
+//multiple Variables
+Console.WriteLine("multiple variables");
+int h = 5, v = 6, n = 50;
+Console.WriteLine(h + v + n);
+
+//My multiple Variables
+Console.WriteLine("my multiple variable");
+int d = 5, f = 3, r = 20;
+Console.WriteLine(d + f + r);
+
+
+//display variables
+Console.WriteLine("Display variables");
+string firstNames = "john";
+string lastNames = "Doe";
+string fullNames = firstNames + lastNames;
+Console.WriteLine("Hello" + fullNames);
+
+int x = 5;
+int y = 6;
+Console.WriteLine(x + y);
+
+//my display variables
+Console.WriteLine("My display variables");
+string Names = "james";
+string lastName = "john";
+string full = Names + lastName; 
+Console.WriteLine("Hello" + full); 
+
+int a = 6;
+int b = 8;
+Console.WriteLine(a + b);
+
+
+// variables
+Console.WriteLine("variable");
+
+string name = "john";
+Console.WriteLine(name);
+
+int myNum = 15;
+Console.WriteLine(myNum);
+
+double myDoubleNum = 5.99;
+Console.WriteLine(myDoubleNum);
+
+char myLetter = 'D';
+Console.WriteLine(myLetter);
+
+bool myBool = true;
+Console.WriteLine(myBool);
+// My variables
+Console.WriteLine("My variable");
+string Name = "james";
+Console.WriteLine(Name);
+
+int myNumb = 20;
+Console.WriteLine(myNumb);
+
+double myDouble = 4.66;
+Console.WriteLine(myDouble);
+
+char myLetters = 'R';
+Console.WriteLine(myLetters);
+
+bool myBools = false;
+Console.WriteLine(myBools);
+ 
+//output
+Console.WriteLine("output");
+Console.WriteLine("Hello World");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine("It is awsome!");
+// My output
+Console.WriteLine("my output");
+Console.WriteLine("My first c# code");
+Console.WriteLine("output");
+
+
+
+
+
+
+//Console.WriteLine("addition");
+Console.WriteLine(2+3);
+
+Console.WriteLine("division");
+Console.WriteLine(2/9);
+
+Console.WriteLine("multiplication");
+Console.WriteLine(20*9);
+
+Console.WriteLine("modulus");
+Console.WriteLine(2%60);
+
+Console.WriteLine("subtraction");
+Console.WriteLine(40-3);
+
+
+Console.WriteLine("it returns the square root numbers");
+Console.WriteLine(Math.Sqrt(10));
+Console.WriteLine("it returns the positive numbers");
+Console.WriteLine(Math.Abs(10));
+Console.WriteLine("it returns the smaller numbers");
+Console.WriteLine(Math.Min(2, 10));
+Console.WriteLine( "it returns the larger numbers");
+Console.WriteLine(Math.Max(2, 10));
+
+
+
+
